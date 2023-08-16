ec8a5001856053b6ae29fa84c7584611f6e12780
prasaSL <prasadmadushan09@gmail.com>
2023-08-16 18:25:08 +0530

    add the math functions

--- cal.py
+++ cal.py
@@ -1,5 +1,6 @@
 import kivy
 kivy.require('2.1.0')
+import re
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
@@ -17,9 +18,32 @@ class calLayout(Widget):
         if self.ids.input.text=='0':
             self.ids.input.text=''
             self.ids.input.text += str(button)
+        elif self.ids.input.text[-1] in ['+', '-', '*', '/'] and button == 0:
+            pass
         else:
             self.ids.input.text += str(button)
-
+    def pressBtnSign(self, sign):
+        if self.ids.input.text[-1] in ['+', '-', '*', '/']:
+            pass
+        else:
+            self.ids.input.text += sign
+    def pressBtnminus(self):
+        text = self.ids.input.text
+        if "-" in text:
+            self.ids.input.text = text.replace("-", "")
+        else:
+            self.ids.input.text = '-' + self.ids.input.text
+    def pressBtnDot(self):
+        list1=re.split('\+|\-|\*|\/|\%',self.ids.input.text)
+        if len(list1)!=0  and not '.' in list1[-1] :
+            self.ids.input.text += '.'
+        elif "." not in self.ids.input.text and self.ids.input.text[-1] != ['+', '-', '*', '/']:
+            self.ids.input.text += '.'
+        else:
+            pass
+    def pressBtnEqual(self):
+        answer = eval(self.ids.input.text)
+        self.ids.input.text = str(answer)
 class calApp(App):
     def build(self):
         return calLayout()
