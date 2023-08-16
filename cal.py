ecfbd5ee6bd74ffa9a22a7fbd7cb4b7c8d1c8a9a
prasaSL <prasadmadushan09@gmail.com>
2023-08-16 20:18:45 +0530

    update math function and add come comments

--- cal.py
+++ cal.py
@@ -6,14 +6,19 @@ from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.core.window import Window
 
+#set window size
 Window.size = (500, 600)
 
+#load kv file
 Builder.load_file('cal.kv')
 
+#main class
 class calLayout(Widget):
+
+    #clear input
     def clear(self):
         self.ids.input.text = '0'
-    
+    #press number button
     def pressBtnNumber(self, button):
         if self.ids.input.text=='0':
             self.ids.input.text=''
@@ -22,14 +27,16 @@ class calLayout(Widget):
             pass
         else:
             self.ids.input.text += str(button)
-            
+
+
+    #press sign button       
     def pressBtnSign(self, sign):
         if self.ids.input.text[-1] in ['+', '-', '*', '/']:
             pass
         else:
             self.ids.input.text += sign
 
-            
+     #press Mainers button       
     def pressBtnminus(self):
         if self.ids.input.text[-1] in ['+', '-', '*', '/'] or self.ids.input.text=='0':
             pass
@@ -38,6 +45,7 @@ class calLayout(Widget):
         else:
             self.ids.input.text = f'-{self.ids.input.text}'
 
+    #press decimal place  button
     def pressBtnDot(self):
         list1=re.split('\+|\-|\*|\/|\%',self.ids.input.text)
         if len(list1)!=0  and not '.' in list1[-1] :
@@ -48,13 +56,21 @@ class calLayout(Widget):
             pass
 
 
-
+    #press equal button
     def pressBtnEqual(self):
         answer = eval(self.ids.input.text)
         self.ids.input.text = str(answer)
+
+    #press backspace button
+    def backspace(self):
+        self.ids.input.text = self.ids.input.text[:-1]
+
+#main app
 class calApp(App):
     def build(self):
         return calLayout()
+
+#run app
 
 if __name__ == '__main__':
     calApp().run()
